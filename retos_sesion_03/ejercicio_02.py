b54f05ac48ef6ab61d0ccb3c21410ecf23bdbe18
Alicia Hurtado <alicia.cameliah@gmail.com>
2025-05-04 01:34:11 -0400

    Diálogo entre una persona y su gato

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,9 @@
+# Diálogo entre una persona y su gato 🐱
+print("Persona: ¡Hola, gatito! ¿Cómo estás hoy?")
+print("Gato: Miau... Estoy bien, pero tengo hambre.")
+print("Persona: ¿Quieres comer algo? Tengo tu comida favorita.")
+print("Gato: Miiiauu, sí, dame comida ahora mismo.")
+print("Persona: Jajaja, está bien, aquí tienes.")
+print("Gato: Miau, gracias humano. ¡Ahora me voy a dormir!")
+print("Persona: Descansa, pequeño.")
+print("Gato: Zzzz...")
