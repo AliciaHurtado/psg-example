00b6d69bf404716d94c8c47c8264e32d8d7f741c
Alicia Hurtado <alicia.cameliah@gmail.com>
2025-06-07 20:35:01 -0400

    ejercicio_04: verificación de número par

---
+++ retos_sesion_06/ejercicio_04.py
@@ -0,0 +1,9 @@
+#La resta de los números 17 y 9 es un número par?
+# Restamos los números
+resultado = 17 - 9
+
+# Verificamos si es par (número divisible entre 2)
+es_par = resultado % 2 == 0
+
+print("Resultado de la resta:", resultado)
+print("¿Es un número par?", es_par)
