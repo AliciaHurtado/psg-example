d14c2c92cb8e91f57770c4e39fac74340076ee95
Alicia Hurtado <alicia.cameliah@gmail.com>
2025-06-07 21:59:03 -0400

    ejercicio 8 resta de tres enteros

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,10 @@
+# Pedir tres números enteros al usuario
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+c = int(input("Ingrese el tercer número: "))
+
+# Realizar la resta
+resultado = a - b - c
+
+# Mostrar el resultado
+print("El resultado de la resta es:", resultado)
