2b85443dee28ecd3eded41995431d167cdd80414
Alicia Hurtado <alicia.cameliah@gmail.com>
2025-06-07 20:45:07 -0400

    ejercicio_07: comparación entre cuadrado y suma

---
+++ retos_sesion_06/ejercicio_07.py
@@ -0,0 +1,9 @@
+#¿El cuadrado y la suma de 2 y 2 son iguales?
+cuadrado = 2 ** 2       # 2 al cuadrado
+suma = 2 + 2            # suma de 2 y 2
+
+son_iguales = cuadrado == suma
+
+print("2^2 =", cuadrado)
+print("2 + 2 =", suma)
+print("¿Son iguales?", son_iguales)
