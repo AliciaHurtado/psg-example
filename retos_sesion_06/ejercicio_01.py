f1e7c64dae461184728899c97c49e98f8a486667
Alicia Hurtado <alicia.cameliah@gmail.com>
2025-06-07 20:22:46 -0400

    ejercicio_01: evaluación de valor booleano

---
+++ retos_sesion_06/ejercicio_01.py
@@ -0,0 +1,6 @@
+# ejercicio_01.py
+#¿Si se declara una variable boolean del valor 0.0000000001 es True o False?
+valor = 0.0000000001
+booleano = bool(valor)
+print("Valor:", valor)
+print("¿Es True o False?", booleano)
